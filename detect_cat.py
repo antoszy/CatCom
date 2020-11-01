41d511f857cbee5362c11c71e5f4d69e759cfb14
Michal Antoszkiewicz <michal.antoszkiewicz@gmail.com>
2020-11-01 19:16:04 +0000

    Add directory creation to detct_cat script
    Signed-off-by: Michal Antoszkiewicz <michal.antoszkiewicz@gmail.com>

--- detect_cat.py
+++ detect_cat.py
@@ -1,24 +1,32 @@
 from datetime import datetime
+import os
 from rpi import rec_utils
 import utils.audio_utils as au
-from pathlib import Path
+import numpy as np
 
-DIRECTORY = "records_loud"
+DIRECTORY = "records_loud_90"
+REC_LENGTH = 50
 
-file_counter = au.get_initial_file_no(DIRECTORY, "rec_a")
-with rec_utils.AudioRecorder() as audio_rec:
-    print(audio_rec)
-    while(1):
-        audio_chank = audio_rec.read_chank()[1]
-        if (au.check_threshold(audio_chank, norm_tsh=0.475)):
-            print("Loud audio detected at:")
-            print(datetime.now().time())
-            record = []
-            record.extend(audio_chank)
-            for sample_num in range(0,100):
-                audio_chank = audio_rec.read_chank()[1]
-                record.extend(audio_chank)
-            au.save_wav(DIRECTORY+"/rec_a"+str(file_counter).zfill(5)+".wav",
-                        record,
-                        audio_rec.sample_rate)
+if __name__ == "__main__":
+    if not os.path.exists(DIRECTORY):
+        print(f"Creating directory {DIRECTORY}")
+        os.makedirs(DIRECTORY)
+        file_counter = 0
+    else:
+        file_counter = au.get_initial_file_no(DIRECTORY, "rec_a")
+
+    with rec_utils.AudioRecorder() as audio_rec:
+        while(1):
+            audio_chank = audio_rec.read_chank()[1]
+            if (au.check_threshold(audio_chank, norm_tsh=0.98)):
+                print("Loud audio detected at:")
+                print(datetime.now().time())
+                record = np.array(audio_chank, dtype=np.int16)
+                for sample_num in range(0, REC_LENGTH):
+                    audio_chank = audio_rec.read_chank()[1]
+                    record = np.append(record, audio_chank)
+                au.save_wav(DIRECTORY+"/rec_a"+str(file_counter).zfill(5)+".wav",
+                            record,
+                            audio_rec.sample_rate)
+                file_counter += 1
 
