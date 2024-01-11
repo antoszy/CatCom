97a2e1942f4f9b7df3b9f1419146b00da2564e43
Michal Antoszkiewicz <mantoszkiewicz@flyps.io>
2024-01-11 08:43:53 +0100

    Add dockerfile for environment for the neural net training
    Signed-off-by: Michal Antoszkiewicz <mantoszkiewicz@flyps.io>

--- train_neural_net/start_docker.sh
+++ train_neural_net/start_docker.sh
@@ -3,8 +3,8 @@
 # Run a Docker container with TensorFlow GPU and Jupyter Notebook.
 # Map port 8080 from the host to port 8080 inside the container.
 # Mount the current directory (.) to the /notebooks directory inside the container.
-# docker run -it --rm -p 8080:8080 -v "$(pwd)":/notebooks tensorflow/tensorflow:latest-gpu-jupyter \
+# docker run -it --rm -p 8080:8080 -v "$(pwd)":/notebooks tensorall \
 #     bash 
 
-docker run --gpus all -it --rm -p 8080:8080 -v "$(pwd)":/notebooks tensorflow/tensorflow:latest-gpu-jupyter \
+docker run --gpus all -it --rm -p 8080:8080 -v "$(pwd)":/notebooks tensorall \
     bash -c "cd /notebooks && jupyter notebook --ip=0.0.0.0 --port=8080 --allow-root"
