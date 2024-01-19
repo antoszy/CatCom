5e40645161b53792c324f39ae4247f5e005c5b64
Michal Antoszkiewicz <mantoszkiewicz@flyps.io>
2024-01-19 20:33:11 +0100

    Fix neural net cat detector

--- neural_net/app/model.py
+++ neural_net/app/model.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_io as tfio
 
+SCORE_THRESHOLD = 0.3
+
 
 class CatDetector:
     def __init__(self):
@@ -16,11 +18,11 @@ class CatDetector:
         self.class_names = self._load_class_mapping(self.yamnet_model)
         self.cat_class_index = self.class_names.index('Cat')
 
-    def _load_yamnet_model():
+    def _load_yamnet_model(self):
         yamnet_model_handle = 'https://tfhub.dev/google/yamnet/1'
         return hub.load(yamnet_model_handle)
 
-    def _load_class_mapping(yamnet_model):
+    def _load_class_mapping(self, yamnet_model):
         class_map_path = yamnet_model.class_map_path().numpy().decode('utf-8')
         class_names =list(pd.read_csv(class_map_path)['display_name'])
         return class_names
@@ -30,7 +32,9 @@ class CatDetector:
         class_scores = tf.reduce_mean(scores, axis=0)
         top_class = tf.math.argmax(class_scores)
         inferred_class = self.class_names[top_class]
-        print(f"{float(class_scores[76]):.3f}")
+        #print(f"{float(class_scores[76]):.3f}")
+        return float(class_scores[76]) > SCORE_THRESHOLD
+
 
 
 @tf.function
